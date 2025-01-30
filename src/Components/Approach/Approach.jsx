a172535603eab6041aecd65672c943ec1c57ab73
Regis <regis.enama@gmail.com>
2025-01-30 18:41:13 -0500

    updated the approach

--- src/Components/Approach/Approach.jsx
+++ src/Components/Approach/Approach.jsx
@@ -1,24 +1,23 @@
 import "../../CSS/home.css" 
 import "../Approach/Approach.scss"
-import logo from "../../assets/logo.jpeg"
+import logo from "../../assets/anchor2.png"
 
 export default function Approach (){ 
     return ( 
         <>
         <div className="approach">
-                <h1 className="title"> Our Approach</h1>
                 <div className="card">
                     <div className="left">
-                        <div className="description">
-                            <p>                               
-                            </p>
-                        </div>
+                        <h1 className="title"> Our Approach</h1>
+                        <p> 
+                            Established in 2020, Anchor Academy seeks to provide young athletes with the necessary soccer skills required for their placement and success on teams at the highschool, club, and collegiate level.  
+                            <br />
+                            We make sure to take a slow and detailed approach when building up skill, making sure no steps are skipped and that our players recieve the best, in depth training out of our coaches.                        
+                        </p>
                     </div>
 
                     <div className="right">
-                        <div className="logo">
-                            <img src={logo} alt="" />
-                        </div>
+                        <img src={logo} alt="" />
                     </div>
                 </div>
 
--- src/Pages/Home_page/home_page.jsx
+++ src/Pages/Home_page/home_page.jsx
@@ -1,7 +1,8 @@
 import Navbar from "../../Components/Navbar/Navbar";
 import "../../CSS/home.css";
 import soccervid from "../../assets/IMG_3158.mp4";
-import Slider from "../../Components/Img_slider/Slider.jsx";
+import Approach from "../../Components/Approach/Approach";
+// import Slider from "../../Components/Img_slider/Slider.jsx";
 import logo from "../../assets/anchor2.png"
 
 export default function Home() {
@@ -15,31 +16,7 @@ export default function Home() {
                     <h1 className="title"> Anchor Academy</h1>
                 </div>
 
-                <div className="approach">
-                <h1 className="title"> Our Approach</h1>
-                <div className="card">
-                    <div className="left">
-                        <div className="description">
-                            <p>    
-                                Established in 2020, Anchor Academy seeks to provide personal training to the future soccer stars of tomorrow                           
-                            </p>
-                        </div>
-                    </div>
-
-                    <div className="right">
-                        <div className="logo">
-                            {
-                                <img src={logo} alt="" />
-                            }
-                        </div>
-                    </div>
-                </div>
-
-                </div>
-                
-                {/* <div className="slider">
-                    <Slider/>
-                </div> */}
+                <Approach/>
 
             </div>
             
